9f5e3bbded6604217237ee820778e6acc3d700c0
Paolo Ceravolo <paoloceravolo@cln-243-200.eduroam.unimi.it>
2019-04-12 10:23:25 +0200

    A clock with one function

--- clock.js
+++ clock.js
@@ -4,7 +4,7 @@ var date = new Date();
 var h = date.getHours(); // 0 - 23
 var m = date.getMinutes(); // 0 - 59
 var s = date.getSeconds(); // 0 - 59
-var meridiem = "AM";
+var meridien = "AM";
     
     if(h == 0){
         h = 12;
@@ -12,7 +12,7 @@ var meridiem = "AM";
     
     if(h > 12){
         h = h - 12;
-        meridiem = "PM";
+        meridien = "PM";
     }
     
     // The conditional ternary operator is the only JavaScript operator that takes three operands. This operator is frequently used as a shortcut for the if statement.
@@ -20,7 +20,7 @@ var meridiem = "AM";
     m = (m < 10) ? "0" + m : m;
     s = (s < 10) ? "0" + s : s;
     
-    var currTime = h + ":" + m + ":" + s + " " + meridiem;
+    var currTime = h + ":" + m + ":" + s + " " + meridien;
     //console.log(time);
     document.getElementById("myclock").innerText = currTime ;
     //document.getElementById("myclock").textContent = time;

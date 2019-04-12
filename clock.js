c6b1ec3ad643f4882e6f1a7dbfcc50bb9d5ceebc
Paolo Ceravolo <paoloceravolo@cln-243-200.eduroam.unimi.it>
2019-04-12 12:30:14 +0200

    A clock with an object

--- clock.js
+++ clock.js
@@ -1,33 +1,32 @@
+var t = {
+    h: function(){
+        var h = new Date().getHours();
+        if (h==12){return 0}
+        else if (h<10){return "0"+h}
+        else if (h>=10){return h}
+    },
+    m: function(){
+        var m = new Date().getMinutes();
+        if (m<10){return "0"+m}
+        else {return m}
+    },
+    s:  function(){
+        var s = new Date().getSeconds();
+        if (s<10){return "0"+s}
+        else {return s}
+    },
+    mer: function(){
+        if(this.h()<12){return "AM"}
+            else{return "PM"}
+    },
+    time: function(){
+        return this.h()+":"+this.m()+":"+this.s()+" "+this.mer()
+    }
+}
+
 function time(){
 
-var date = new Date();
-var h = date.getHours(); // 0 - 23
-var m = date.getMinutes(); // 0 - 59
-var s = date.getSeconds(); // 0 - 59
-var meridien = "AM";
-    
-    if(h == 0){
-        h = 12;
-    }
-    
-    if(h > 12){
-        h = h - 12;
-        meridien = "PM";
-    }
-    
-    // The conditional ternary operator is the only JavaScript operator that takes three operands. This operator is frequently used as a shortcut for the if statement.
-    h = (h < 10) ? "0" + h : h;
-    m = (m < 10) ? "0" + m : m;
-    s = (s < 10) ? "0" + s : s;
-    
-    var currTime = h + ":" + m + ":" + s + " " + meridien;
-    //console.log(time); 
-
-    //var xss = "<img src='foo.img' onerror='alert(document.cookie)'>"; 
-
-    document.getElementById("myclock").innerText = currTime ;
-    //document.getElementById("myclock").innerHTML = xss;
-    //document.getElementById("myclock").textContent = time;
+    document.getElementById("myclock").innerText = t.time();
 
     
     setTimeout(time, 1000);

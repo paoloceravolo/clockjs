752403ac12194ac98ad6a08016f3c9ecf034dd04
Paolo Ceravolo <paoloceravolo@cln-247-35.eduroam.unimi.it>
2019-05-23 10:17:57 +0200

    RORO desing pattern

--- ajax.js
+++ ajax.js
@@ -1,12 +1,19 @@
 // Disable Same origin policy
 //open -n -a /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --args --user-data-dir="/tmp/chrome_dev_sess_1" --disable-web-security
 
-// Script parameters
-const target = '#content';
-const wrapper1 = 'article';
-const wrapper2 = 'p';
-const ajaxMethod = 'GET';
-const ajaxUrl = 'https://www.dati.lombardia.it/resource/5bbn-8w23.json';
+// Script parameters usin RORO pattern: Receive Objects Return Objects 
+const templateObj = {
+  target: '#content',
+  wrapper1: 'article',
+  wrapper2: 'p',
+}
+
+const ajaxcallObj = {
+  method: 'GET',
+  url: 'https://www.dati.lombardia.it/resource/5bbn-8w23.json',
+  asynchronous: true,
+}
+
 
 //The experimental pipeline operator |> (currently at stage 1) pipes the value of an expression into a function. This allows the creation of chained function calls in a readable manner. The result is syntactic sugar in which a function call with a single argument can be written like this:
 //callAjax |> template;
@@ -15,12 +22,12 @@ const ajaxUrl = 'https://www.dati.lombardia.it/resource/5bbn-8w23.json';
 //callAjax();
 // option 2 use calls to pipe: call the funtion a pass its output to a subsequent function.
 // this does not match with the asynchronous pattern 
-//template(callAjax());
+//callAjax(template());
 // option 3 call the function and its callback. Callabck will execute when the asynchronous event is complete
-callAjax(template);
+callAjax(ajaxcallObj, templateObj, template);
 
 
-function callAjax(callback){
+function callAjax(call, temp, callback){
 //instanzo l'oggetto XMLHttpRequest
   var xhr = new XMLHttpRequest();
   //debugger;
@@ -34,23 +41,23 @@ xhr.onreadystatechange = function(){
       // option 2 use calls to pipe: call the funtion a pass its output to a subsequent function.
       //return xhr.response;
       // option 3 call the function and its callback. Callabck will execute when the asynchronous event is complete
-      callback(xhr.response);
+      callback(xhr.response, temp);
     }
   }
 }
 //imposto la chiamata HTTP
-xhr.open(ajaxMethod,ajaxUrl,true);
+xhr.open(call.method,call.url,call.asynchronous);
 
 //invio la richiesta
 xhr.send();
 }
 
-function template(response){
+function template(response, temp){
 
       //console.log(JSON.parse(response)[3].id);
       //console.log(target);
 
-      let targ = document.querySelector(target);
+      let targ = document.querySelector(temp.target);
       let resp = JSON.parse(response);
 
       //console.log(resp[3].id);
@@ -60,8 +67,8 @@ function template(response){
        // debugger;
         //targ.innerText += arr[index].id;
         //targ.innerText += item.id;
-        let newEle1 = document.createElement(wrapper1);
-        let newEle2 = document.createElement(wrapper2);
+        let newEle1 = document.createElement(temp.wrapper1);
+        let newEle2 = document.createElement(temp.wrapper2);
         let textCont = document.createTextNode(item.categoria +" - "+ item.comune);
         targ.appendChild(newEle1).appendChild(newEle2).appendChild(textCont);
         //console.log(index);
